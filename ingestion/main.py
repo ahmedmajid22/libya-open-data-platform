98babd3a6ad391921488ec4e940d05e7a8d0e1d6
AhmedMajid22 <ahmedmajidali22@gmail.com>
2026-04-18 13:15:29 +0200

    chore: add ingestion pipeline entrypoint

---
+++ ingestion/main.py
@@ -0,0 +1,9 @@
+"""
+Main entry point for Libya Open Data Pipeline.
+
+This will orchestrate:
+- World Bank extraction
+- UN data ingestion
+- UNHCR data ingestion
+- Load into BigQuery raw layer
+"""
